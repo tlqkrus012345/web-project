5eb29a0a898f2430808a7991c19d1e7651254bfc
tlqkrus012345 <tlqkrus012345@naver.com>
2024-01-07 21:48:36 +0900

    검색 컨트롤러와 서비스 클래스 생성

--- src/main/java/com/server/search/application/SearchService.java
+++ src/main/java/com/server/search/application/SearchService.java
@@ -1,4 +1,10 @@
 package com.server.search.application;
 
+import com.server.search.external.ExternalInterface;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SearchService {
 }
--- src/main/java/com/server/search/presentation/SearchController.java
+++ src/main/java/com/server/search/presentation/SearchController.java
@@ -1,5 +1,17 @@
 package com.server.search.presentation;
 
-public class SearchController {
+import com.server.search.application.CircuitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class SearchController {
+    // 요청 -> 컨트롤러 -> 서킷 서비스-> 서치 서비스-> 외부 api 호출
+    private final CircuitService circuitService;
+    @GetMapping("/search")
+    public void search(@RequestParam("food") String food, @RequestParam("sort") String sort) {
+
+    }
 }
